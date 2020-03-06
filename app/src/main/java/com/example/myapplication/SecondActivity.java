aafffbce3ef5394af656e35cbc1e06b44fbef676
pigeaon <1015139598@qq.com>
2020-03-07 01:13:23 +0800

    基本页面的实现
    202003070113

--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -1,30 +1,81 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
 import com.example.common.app.Activity;
+import com.example.common.widget.PortraitView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class SecondActivity extends Activity {
-    @BindView(R.id.txt_test)
-    TextView m;
+
+    @BindView(R.id.appbar)
+    View mLayAppbar;
+
+    @BindView(R.id.im_portrait)
+    PortraitView mPortrait;
+
+    @BindView(R.id.txt_title)
+    TextView mTitle;
+
+    @BindView(R.id.lay_container)
+    FrameLayout mContainer;
+
+    @BindView(R.id.navigaton)
+    BottomNavigationView mNavigationView;
+
 
     @Override
     protected void initWidget() {
         super.initWidget();
-        m.setText("test");
+        Glide.with(this)
+                .load(R.drawable.bg_src_morning)
+                .centerCrop()
+                .into(new ViewTarget<View, GlideDrawable>(mLayAppbar) {
+                    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                        this.view.setBackground(resource.getCurrent());
+                    }
+                });
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+
+    }
+
+    @OnClick(R.id.im_search)
+    void onSearchMenuClick(){
+
+    }
+
+    @OnClick(R.id.btn_action)
+    void onActionClick(){
+
     }
 
     @Override
     protected int getContentLayoutId() {
         return R.layout.activity_main;
     }
+
+
 }
